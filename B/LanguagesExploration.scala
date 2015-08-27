9bafc8726238aa6f100d369ab10294f9e0e4d18d
Prayogo Cendra <prayogocendra@gmail.com>
2015-08-27 23:06:58 +0700

    Update LanguagesExploration.scala

--- B/LanguagesExploration.scala
+++ B/LanguagesExploration.scala
@@ -1,27 +1,28 @@
 object PublicClass{
 
 	var staticInt: Int = 0
-
 	val PI: Double = 3.14
-
 }
 
 class PublicClass{
 
 	private var privateInt: Int = 0
-
 	var publicString: String = "Hello"
 
-	def setPrivateInt(newPrivateInt: Int) = {privateInt = newPrivateInt}
+	def setPrivateInt(newPrivateInt: Int): Unit = {
+		privateInt = newPrivateInt
+	}
 
-	def getPrivateInt = privateInt
-
+	def getPrivateInt(): Int = {
+		return privateInt
+	}
 }
 
-class PublicSubClass extends PublicClass {
+class PublicSubClass extends PublicClass{
 
-	override def getPrivateInt = 2 * super.getPrivateInt
-	
+	override def getPrivateInt(): Int = {
+		return (2 * super.getPrivateInt())
+	}
 }
 
 object LanguagesExploration {
@@ -31,26 +32,15 @@ object LanguagesExploration {
 	def main(args: Array[String]) {
 
 		val publicClass = new PublicClass
-
-		val publicClass.setPrivateInt(13)
-
-		println("[13] publicClass.getPrivateInt() = " + publicClass.getPrivateInt)
-
+		publicClass.setPrivateInt(13)
+		println("[13] publicClass.getPrivateInt() = " + publicClass.getPrivateInt())
 		publicClass.publicString = "World!"
-
 		println("[World!] publicClass.publicString = " + publicClass.publicString)
-
 		staticInt = 10
-
 		println("[10] PublicClass.staticInt = " + PublicClass.staticInt)
-
 		println("[3.14] PublicClass.PI = " + PublicClass.PI)
-
 		val publicSubClass = new PublicSubClass
-
 		publicSubClass.setPrivateInt(13)
-
-		println("[26] publicSubClass.getPrivateInt() = " + publicSubClass.getPrivateInt)
-		
+		println("[26] publicSubClass.getPrivateInt() = " + publicSubClass.getPrivateInt())
 	}
 }
