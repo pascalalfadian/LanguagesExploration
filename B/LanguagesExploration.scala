131a9acec0f1311c55b6ec76d9575cb4d7284560
Prayogo Cendra <prayogocendra@gmail.com>
2015-08-27 22:53:11 +0700

    Update LanguagesExploration.scala
    Masih ada error

--- B/LanguagesExploration.scala
+++ B/LanguagesExploration.scala
@@ -1,3 +1,56 @@
+object PublicClass{
 
-hai
-Selamat Datang
+	var staticInt: Int = 0
+
+	val PI: Double = 3.14
+
+}
+
+class PublicClass{
+
+	private var privateInt: Int = 0
+
+	var publicString: String = "Hello"
+
+	def setPrivateInt(newPrivateInt: Int) = {privateInt = newPrivateInt}
+
+	def getPrivateInt = privateInt
+
+}
+
+class PublicSubClass extends PublicClass {
+
+	override def getPrivateInt = 2 * super.getPrivateInt
+	
+}
+
+object LanguagesExploration {
+
+	import PublicClass._
+
+	def main(args: Array[String]) {
+
+		val publicClass = new PublicClass
+
+		val publicClass.setPrivateInt(13)
+
+		println("[13] publicClass.getPrivateInt() = " + publicClass.getPrivateInt)
+
+		publicClass.publicString = "World!"
+
+		println("[World!] publicClass.publicString = " + publicClass.publicString)
+
+		staticInt = 10
+
+		println("[10] PublicClass.staticInt = " + PublicClass.staticInt)
+
+		println("[3.14] PublicClass.PI = " + PublicClass.PI)
+
+		val publicSubClass = new PublicSubClass
+
+		publicSubClass.setPrivateInt(13)
+
+		println("[26] publicSubClass.getPrivateInt() = " + publicSubClass.getPrivateInt)
+		
+	}
+}
