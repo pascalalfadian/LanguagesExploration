09fec946e0f8baeb904afdd3f150eda0ba2f1e3a
louisemarcho <7314002@student.unpar.ac.id>
2015-08-31 09:51:39 +0700

    nomor 7 - 10 nih

--- A/LanguagesExploration.scala
+++ A/LanguagesExploration.scala
@@ -9,4 +9,27 @@ class PublicClass {
 
   // 6. Cara mendefinisikan konstruktor
   // 'Primary Contructor' telah dibuat saat pendefinisian kelas di baris ke-2
+
+  // 7a. Cara mendefinisikan method dengan parameter tanpa return value
+  // 7b. Cara menuliskan dokumentasi untuk method 7a
+
+  /** Mengubah nilai atribut privateInt
+   *  @param privateInt nilai privateInt yang baru.
+   */
+  def setPrivateInt_=(privateInt: Int):Unit {
+    // 8. Cara mengisi atribut dari dalam kelas itu sendiri
+    this.privateInt = privateInt
+  }
+
+  // 9a. Cara mendefinisikan method dengan return value
+  // 9b. Cara menuliskan dokumentasi untuk method 9a
+
+  /**
+   * Getter for the private integer variable.
+   * @return _privateInt
+   */
+  def getPrivateInt() : Int = {
+    // 10. Cara mengembalikan nilai
+    return this.privateInt
+  } 
 }
