87bbbdb0bd6e5951029fddaded8813240967c58b
Keenan Leman <kenl@kenl.name>
2015-08-31 09:45:23 +0700

    Menambah folder A dan menjawab nomor 1 - 3 dan nomor 6

---
+++ A/LanguagesExploration.scala
@@ -0,0 +1,12 @@
+// 1. Cara mendefinisikan kelas
+class PublicClass {
+
+  // 2. Cara mendefiniskan atribut privat bertipe int
+  private var privateInt : Int
+
+  // 3. Cara mendefinisikan atribut public bertipe string, dan mengisi, dengan nilai awal
+  var publicString : String
+
+  // 6. Cara mendefinisikan konstruktor
+  // 'Primary Contructor' telah dibuat saat pendefinisian kelas di baris ke-2
+}
