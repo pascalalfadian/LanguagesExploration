6723b40b5fdbdb9f0cf764e1fab90bfda05086a0
RikiRusli <7314032@student.unpar.ac.id>
2015-09-02 09:05:17 +0700

    Create LanguageExloration.php
    masukin pertama, blom jadi apa-apa

---
+++ A/LanguageExloration.php
@@ -0,0 +1,23 @@
+<html>
+	<head></head>
+	<body>
+		<?php
+			public class PublicClass{
+				private $privateInt;
+				public $publicString="Hello";
+				//public static teuing
+				//public static final
+				public PublicClass(){
+					
+					//kosong
+				}
+				
+			}
+			function setPrivateInt($input) {
+				this->$privateInt=$input;
+			}
+		?>
+	</body>
+	
+
+</html>
