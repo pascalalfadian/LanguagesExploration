56b88b864d612342efc384501e6704f0c28fbdb6
Keenan Leman <kenl@kenl.name>
2015-09-03 22:15:43 +0700

    Fixed: Nomor 2 dan 3

--- A/LanguagesExploration.scala
+++ A/LanguagesExploration.scala
@@ -2,10 +2,10 @@
 class PublicClass {
 
   // 2. Cara mendefiniskan atribut privat bertipe int
-  private var privateInt : Int
+  private var privateInt : Int = _
 
   // 3. Cara mendefinisikan atribut public bertipe string, dan mengisi, dengan nilai awal
-  var publicString : String
+  var publicString : String = _
 
   // 6. Cara mendefinisikan konstruktor
   // 'Primary Contructor' telah dibuat saat pendefinisian kelas di baris ke-2
