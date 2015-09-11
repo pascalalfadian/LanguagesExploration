2d4431137476da7a0cabe399df6d3b91d3f87d86
calvintorsa <7314076@student.unpar.ac.id>
2015-09-11 09:49:08 +0700

    Update LanguagesExploration.rb

--- B/LanguagesExploration.rb
+++ B/LanguagesExploration.rb
@@ -1,5 +1,14 @@
-a
-b
-
-b = 10;
-print b;
+// 1. Cara mendifinisikan kelas
+class PublicClass {
+    
+    // 2. Cara mendefinisikan atribut privat bertipe int
+    @privateInt
+    
+    // 3. Cara mendifinisikan atribut public bertipe string, dan mengisi dengan nilai awal
+    @publicString = "Hello";
+    
+    // 4. Cara mendefinisikan atribut statik
+    @@staticInt;
+    
+    // 5. Cara mendefinisikan konstanta
+    PI = 3.14;
