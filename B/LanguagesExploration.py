c93e1f52ba4ec268641e1b720562e9988620b87b
Andy Setiawan Haryanto <andysetiawan.h@gmail.com>
2015-09-12 11:39:10 +0700

    Update LanguagesExploration.py

--- B/LanguagesExploration.py
+++ B/LanguagesExploration.py
@@ -1,7 +1,19 @@
-# Cara mendefinisikan kelas
+# 1. Cara mendefinisikan kelas
 class PublicClass:
-  # konstruktor 
+  # 6. Cara mendefinisikan konstruktor 
   def __init__(self):
+    # 2. Cara mendefiniskan atribut bertipe int
     self.privateInt=0
-    self.publicString="hello"
+    
+    # 3. Cara mendefinisikan atribut bertipe string, dan mengisi dengan nilai awal
+    self.publicString="Hello"
+    
+    # 5.Cara mendefinisikan konstanta
     self.PI=3.14
+
+# 7a. Cara mendefinisikan method dengan parameter tanpa return value
+# 7b. Cara menuliskan dokumentasi untuk method 7a
+
+def setPrivateInt(self, privateInt):
+    # 8. Cara mengisi atribut dari dalam kelas itu sendiri
+    self.privateInt=privateInt
