b1c930b4f771ab9c9af30e018dbf9eea6434563d
Jovanka Helen Maradenia <jovankalubis@gmail.com>
2015-09-12 07:33:41 +0700

    Update LanguagesExploration.py

--- LanguagesExploration.py
+++ LanguagesExploration.py
@@ -1,18 +1,18 @@
 # 1. Cara mendifinisikan kelas
-class PublicClass():
+class PublicClass:
 
     # 2. Cara mendefinisikan atribut privat bertipe int
-    privateInt = 0;
+    privateInt = 0
 
     # 3. Cara mendifinisikan atribut public bertipe string, dan mengisi dengan nilai awal
-    publicString = "Hello";
+    publicString = "Hello"
 
     # 4. Cara mendefinisikan atribut statik
-    staticInt = 0;
+    staticInt = 0
 
     # 5. Cara mendefinisikan konstanta
-    """static final double PI = 3.14;
-    atribut final tidak dapat didefinisikan dalam bahasa python"""
+    PI = 3.14
+    
 
 
     # 6. Cara mendefinisikan konstruktor
@@ -22,26 +22,16 @@ class PublicClass():
 
     # 7a. Cara mendefinisikan method dengan parameter tanpa return value
     # 7b. Cara menuliskan dokumentasi untuk method 7a
-    '''
-     Mengubah nilai atribut privateInt
-     @param privateInt nilai privateInt yang baru.
-     ''' #error kenapa ya?
-     
     def setPrivateInt(self, privateInt):
         # 8. Cara mengisi atribut dari dalam kelas itu sendiri
-        self.privateInt = privateInt;
+        self.privateInt = privateInt
 
 
     # 9a. Cara mendefinisikan method dengan return value
     # 9b. Cara menuliskan dokumentasi untuk method 9a
-    '''
-     Mendapatkan nilai dari atribut privateInt
-     @return nilai dari privateInt
-     '''
-
     def getPrivateInt(self):
         # 10. Cara mengembalikan nilai
-        return privateInt;
+        return privateInt
 
 
 # 11. Cara menurunkan kelas
@@ -50,40 +40,40 @@ class PublicSubClass(PublicClass):
     # 12. Cara melakukan override method
     def getPrivateInt(self):
         # 13. Cara mengakses method pada superclass
-        return 2 * super.getPrivateInt();
+        return 2 * getPrivateInt()
 
 
 
 class LanguagesExploration():
     def main(args):
         # 14. Cara menginstansiasi kelas baru
-        publicClass = PublicClass();
+        publicClass = PublicClass()
 
         # 15. Cara memanggil method dengan parameter
-        publicClass.setPrivateInt(13);
+        publicClass.setPrivateInt(13)
 
         # 16. Cara memanggil method dan mendapatkan return value-nya
-        print("[13] publicClass.getPrivateInt() = " + publicClass.getPrivateInt());
+        print "[13] publicClass.getPrivateInt() = " , publicClass.getPrivateInt()
 
         # 17. Cara mengubah isi atribut publik dari luar kelas
-        publicClass.publicString = "World!";
+        publicClass.publicString = "World!"
 
         # 18. Cara mendapatkan isi atribut publik dari luar kelas
-        print("[World!] publicClass.publicString = " + publicClass.publicString);
+        print "[World!] publicClass.publicString = " , publicClass.publicString
 
         # 19. Cara mengisi nilai atribut statik
         PublicClass.staticInt = 10;
 
         # 20. Cara mendapatkan nilai atribut statik
-        print("[10] PublicClass.staticInt = " + PublicClass.staticInt);
+        print "[10] PublicClass.staticInt = " , PublicClass.staticInt
 
         # 21. Cara mendapatkan nilai konstanta
-        print("[3.14] PublicClass.PI = " + PublicClass.PI);
+        print "[3.14] PublicClass.PI = " ,PublicClass.PI
 
         # 22. Cara menginstansiasi kelas dengan turunannya
-        publicSubClass = PublicSubClass();
+        publicSubClass = PublicSubClass()
 
-        publicSubClass.setPrivateInt(13);
+        publicSubClass.setPrivateInt(13)
 
         # 23. Memastikan method overriding bekerja
-        print("[26] publicSubClass.getPrivateInt() = " + publicSubClass.getPrivateInt()); #yg ini juga?
+        print "[26] publicSubClass.getPrivateInt() = " ,publicSubClass.getPrivateInt() 
