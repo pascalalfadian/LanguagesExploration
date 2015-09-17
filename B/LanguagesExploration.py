3b8e9a132da5508f50e0a436725ab1ff86152e68
KelvinTandika <kelvintandika@gmail.com>
2015-09-17 12:23:44 +0700

    Update LanguagesExploration.py

--- B/LanguagesExploration.py
+++ B/LanguagesExploration.py
@@ -32,22 +32,23 @@ class PublicSubClass(PublicClass):
   return 2 * super(PublicSubClass, self).getPrivateInt()
 
 class LanguagesExploration():
-  # 14. Cara menginstansiasi kelas baru
-  publicClass=PublicClass()
-  # 15. Cara memanggil method dengan parameter
-  publicClass.setPrivateInt(13)
-  # 16. Cara memanggil method dan mendapatkan return value-nya
-  print("[13] publicClass.getPrivateInt() = " + publicClass.getPrivateInt())
-  # 17. Cara mengubah isi atribut publik dari luar kelas
-  publicClass.publicString = "World!"
-  # 18. Cara mendapatkan isi atribut publik dari luar kelas
-  print("[World!] publicClass.publicString = " + publicClass.publicString)
-  # 19. Cara mengisi nilai atribut statik 
-  publicClass.staticInt=10
-  # 20. Cara mendapatkan nilai atribut statik 
-  print("[10] publicClass.staticInt = %s"%publicClass.staticInt)
-  # 21. Cara mendapatkan nilai konstanta 
-  print("[3.14] publicClass.PI = %s"%publicClass.PI)
-  # 22. Cara menginstansiasi kelas dengan turunannya
-  publicSubClass = PublicSubClass();
-  publicSubClass.setPrivateInt(13);
+  def main(self):
+    # 14. Cara menginstansiasi kelas baru
+    publicClass=PublicClass()
+    # 15. Cara memanggil method dengan parameter
+    publicClass.setPrivateInt(13)
+    # 16. Cara memanggil method dan mendapatkan return value-nya
+    print("[13] publicClass.getPrivateInt() = " + publicClass.getPrivateInt())
+    # 17. Cara mengubah isi atribut publik dari luar kelas
+    publicClass.publicString = "World!"
+    # 18. Cara mendapatkan isi atribut publik dari luar kelas
+    print("[World!] publicClass.publicString = " + publicClass.publicString)
+    # 19. Cara mengisi nilai atribut statik 
+    publicClass.staticInt=10
+    # 20. Cara mendapatkan nilai atribut statik 
+    print("[10] publicClass.staticInt = %s"%publicClass.staticInt)
+    # 21. Cara mendapatkan nilai konstanta 
+    print("[3.14] publicClass.PI = %s"%publicClass.PI)
+    # 22. Cara menginstansiasi kelas dengan turunannya
+    publicSubClass = PublicSubClass();
+    publicSubClass.setPrivateInt(13);
