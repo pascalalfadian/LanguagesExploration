10f1f97947ec589634c68c801bcd2258cb4057e7
Gabriel Radewa Purba <gabrielradewa@gmail.com>
2015-10-02 11:23:57 +0700

    Update LanguageExploration.php
    Perbaikan menyeluruh, seharusnya sudah bisa dijalankan tanpa ada error sama sekali. :-)

--- B/LanguageExploration.php
+++ B/LanguageExploration.php
@@ -1,14 +1,6 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-
+<?php
         // put your code here
         class PublicClass {
-
             // 2. cara mendifinisikan atribut private bertipe int
             var $privateInt;
             
@@ -17,27 +9,23 @@
             
             // 4. cara mendefinisikan atribut statik
             static $staticInt;
-
             // 5. cara mendefinisikan konstanta
             const PI = 3.14;
-
             // 6. cara mendefinisikan konstruktor
             function PublicClass() {
                 
             }
-
             // 7a. Cara mendefinisikan method dengan parameter tanpa return value
             // 7b. Cara menuliskan dokumentasi untuk method 7a
             /*
               mengubah nilai atribut privateInt
               @param privateInt nilai privateInt yang baru
              */
-            function setPrivateInt($privateInt) {
+            function setPrivateInt($masukan) {
                 //8.Cara mengisi atribut dari dalam kelas itu sendiri
                 global $privateInt;
-                $privateInt = $privateInt;
+				$privateInt=$masukan;
             }
-
             // 9a. Cara mendefinisikan method dengan return value
             // 9b. Cara menuliskan dokumentasi untuk method 9a
             /*
@@ -45,62 +33,20 @@
               @return nilai dari privateInt */
             function getPrivateInt() {
                 // 10. Cara mengembalikan nilai
-                global $privateInt;
+				global $privateInt;
                 return $privateInt;
             }
-
         }
-
-        // 11. cara menurunkan kelas
-        class PublicSubClass extends PublicClass {
-
+		
+		class PublicSubClass extends PublicClass {
             // 12. cara melakukan override method
             function getPrivateInt() {
-
                 // 13. cara mengakses method pada superclass
-                return getPrivateInt();
+                return 2*PublicClass::getPrivateInt();
             }
-
         }
-
-        class LanguagesExploration {
-
-            function main() {
-
-                // 14. cara menginstansiasi kelas baru
-                $publicClass = new PublicClass();
-
-                // 15. Cara memanggil method dengan parameter
-                $publicClass->setPrivateInt(13);
-
-                // 16. Cara memanggil method dan mendapatkan return value-nya
-                echo "[13] $publicClass->getPrivateInt() = " . $publicClass->getPrivateInt() . "<br>";
-
-                // 17. Cara mengubah isi atribut publik dari luar kelas
-                $publicClass->publicString = "World!";
-
-                // 18. Cara mendapatkan isi atribut publik dari luar kelas
-                echo "[World] $publicClass->publicString = " . $publicClass->publicString;
-
-                // 19. Cara mengisi nilai atribut statik
-                $publicClass->staticInt = 10;
-                //PublicClass.staticInt = 10;
-                // 20. Cara mendapatkan nilai atribut statik
-                echo "[10] PublicClass->staticInt = " . PublicClass::$staticInt;
-
-                // 21. Cara mendapatkan nilai konstanta
-                echo "[3.14] PublicClass->PI = " . constant(PublicClass::$PI);
-
-                // 22. Cara menginstansiasi kelas dengan turunannya
-                $publicSubClass = new PublicClass();
-                $publicSubClass->setPrivateInt(13);
-
-                // 23. Memastikan method overriding bekerja
-                echo "[26] $publicSubClass->getPrivateInt()" . $publicSubClass->getPrivateInt();
-            }
-
-        }
-        ?>
-
-    </body>
-</html>
+	$obj=new PublicSubClass();
+	$obj->setPrivateInt(10);
+	echo $obj->getPrivateInt();
+	echo $obj->publicString;
+?>
